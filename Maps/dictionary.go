c625509dab152ff1a6dc8c7d9b3f80be5bab7d4e
Nripesh <nripesh.bhusal@gmail.com>
2026-09-12 20:24:16 +0530

    Maps further

--- Maps/dictionary.go
+++ Maps/dictionary.go
@@ -4,10 +4,16 @@ import "errors"
 
 type Dictionary map[string]string
 
+var ErrorNotFound = errors.New("could not find the word you are looking for")
+
 func (d Dictionary) Search(s string) (string, error) {
 	definitions, ok := d[s]
 	if !ok {
-		return "", errors.New("could not find the word you are looking for")
+		return "", ErrorNotFound
 	}
 	return definitions, nil
+}
+
+func (d Dictionary) Add(key, val string) {
+	d[key] = val
 }
--- Maps/dictionary_test.go
+++ Maps/dictionary_test.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"errors"
 	"testing"
 )
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("testKey", "A test key ")
+	want := "A test key "
+	got, err := dictionary.Search("testKey")
+	if err != nil {
+		t.Fatal("Should have found the Key", err)
+	}
+
+	assertStrings(t, got, want)
+
+}
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
 	t.Run("Unkown word", func(t *testing.T) {
 		_, err := dictionary.Search("me")
-		want := "could not find the word you are looking for"
 
 		if err == nil {
 			t.Fatal("Unknown word is supposed to throw error")
 		}
-		assertStrings(t, err.Error(), want)
+		assertError(t, err, ErrorNotFound)
 
 	})
 	t.Run("Known word", func(t *testing.T) {
@@ -24,6 +37,14 @@ func TestSearch(t *testing.T) {
 
 	})
 
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if !errors.Is(got, want) {
+		t.Errorf("got %s wanted %s", got, want)
+	}
 }
 
 func assertStrings(t testing.TB, got, want string) {
